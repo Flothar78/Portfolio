a7e49f30ff68516f6ae80ed6db07e5d8fa6a2b9a
Flothar78 <lamazeguillaume@hotmail.fr>
2022-07-01 10:17:00 +0200

    Elaborating previous button function

--- portfolio.js
+++ portfolio.js
@@ -22,3 +22,28 @@ function nextPhoto() {
     console.log(slideIndex);
   }
 }
+
+previousButton.addEventListener("click", () => {
+  previousProject();
+});
+
+function previousProject() {
+  let prevSlideIndex = nextSlideIndex - 2;
+  console.log(nextSlideIndex);
+  console.log(prevSlideIndex);
+
+  imagesContainer.style.left = -width * prevSlideIndex + "px";
+  prevSlideIndex--;
+  console.log(prevSlideIndex);
+}
+
+/*function previousProject() {
+  const array = Object.keys(images);
+  let prevSlideIndex = array.forEach((element) => {
+    element = element - 1;
+    console.log(element);
+  });
+  //console.log(prevSlideIndex);
+  imagesContainer.style.left = -width * prevSlideIndex + "px";
+  console.log(imagesContainer.style.left);
+}*/
